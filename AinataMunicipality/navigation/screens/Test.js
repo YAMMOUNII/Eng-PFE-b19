7fd2c9d899c1f88c140fe458c71f92aeb8f3feda
your_usernamerhjeanpierre <rhjeanpierre7@gmail.com>
2022-05-21 11:44:13 +0300

    added home screen

---
+++ AinataMunicipality/navigation/screens/Test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+const Test = () => {
+    return (
+        <Text>
+         This is a test screen
+        </Text>
+    );
+};
+
+export default Test;
